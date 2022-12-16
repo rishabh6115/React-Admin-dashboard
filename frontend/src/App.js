b5c29b949978bf4a92544d92b28d8541bff37235
Rishabh <rishabh7424960527@gmail.com>
2022-12-16 13:43:43 +0530

    Added Admin Page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -16,6 +16,8 @@ import Overview from "./scenes/Overview";
 import Daily from "./scenes/Daily";
 import Monthly from "./scenes/Monthly";
 import Breakdown from "./scenes/Breakdown";
+import Admin from "./scenes/Admin";
+import Performance from "./scenes/Performance";
 
 function App() {
   const mode = useSelector((state) => state.global.mode);
@@ -37,6 +39,8 @@ function App() {
               <Route path="/daily" element={<Daily />} />
               <Route path="/monthly" element={<Monthly />} />
               <Route path="/breakdown" element={<Breakdown />} />
+              <Route path="/admin" element={<Admin />} />
+              <Route path="/performance" element={<Performance />} />
             </Route>
           </Routes>
         </ThemeProvider>
